9ee0a2de7fa38b0e6f13fe3b5156fb6646bcc9a7
Umar9292 <umarpromd@gmail.com>
2020-04-02 15:11:02 +0500

    added an API to log user out.

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const addProductsRouter = require('./src/routes/admin/addProducts');
 const productImageRouter = require('./src/routes/admin/productImage');
 const ordersRouter = require('./src/routes/admin/orders');
 const playerIdRouter = require('./src/routes/admin/playerId');
+const logoutRouter = require('./src/routes/user/logout');
 
 const port = process.env.PORT || 8080;
 
@@ -30,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "views")));
 
-app.use('/user', signUpRouter, profileRouter);
+app.use('/user', signUpRouter, profileRouter, logoutRouter);
 app.use('/products',
     productsRouter,
     pricingRouter,
@@ -49,7 +50,10 @@ mongoose.connect(dbUrl,
     }, (err) => {
         if (err) {
             console.log(err);
-        } else {
+        }
+        else {
+            app.listen(port, () => console.log(`Listening on port ${port}`));
+
             console.log("Connected to databse");
         }
     }
@@ -60,7 +64,5 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
