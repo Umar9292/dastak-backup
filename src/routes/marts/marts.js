b7e70de12b405eb4aa00f7cdc28077d5c28fb813
Umar9292 <umarpromd@gmail.com>
2020-04-14 19:57:26 +0500

    updated all marts api

--- src/routes/marts/marts.js
+++ src/routes/marts/marts.js
@@ -36,7 +36,9 @@ router.post('/addMart', async (req, res) => {
 
 router.get("/allMarts", async (req, res) => {
     try {
-        const allMarts = await Marts.find().sort({ name: 1 }).select('-password -__v');
+        const allMarts = await Marts.find({ type: 'admin' })
+            .sort({ name: 1 })
+            .select('-password -__v');
 
         return res.json({
             status: '200',
