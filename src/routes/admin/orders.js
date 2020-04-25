f98f30f4c183c93e633151004ac158696cee20f2
Abdul Haseeb <Haseeb@healrthrix.com>
2020-04-25 17:12:55 +0500

    alert message changed

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -137,7 +137,7 @@ router.post('/changeOrderStatus', async (req, res) => {
         }
 
         if (req.body.status === 'Shipped') {
-            const msg = `Dear ${user.name} your order# ${req.body.orderNum} has been shipped and will arrive in approximately 1 hour.`;
+            const msg = `Dear ${user.name} your order# ${req.body.orderNum} has been shipped and will arrive in approximately 30 mins.`;
 
             await notify.user(msg, user.playerId, { flag: 'orderShipped' });
         }
