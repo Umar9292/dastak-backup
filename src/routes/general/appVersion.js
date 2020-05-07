353e83ddc12b264e79a11f36da80cad21c78e442
Abdul Haseeb <Haseeb@healrthrix.com>
2020-05-07 17:47:07 +0500

    app version update

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -5,18 +5,18 @@ router.post('/checkVersion', async (req, res) => {
     try {
         const body = req.body;
 
-        if (body.platform === 'ios' && body.version === '1.2.0') return res.json({ status: '200' });
+        if (body.platform === 'ios' && body.version === '1.2.1') return res.json({ status: '200' });
 
-        if (body.platform === 'ios' && body.version !== '1.2.0') {
+        if (body.platform === 'ios' && body.version !== '1.2.1') {
             return res.json({
                 status: '404',
                 msg: `A new update is now available. kindly update your App to get the best experience`
             });
         }
 
-        if (body.platform === 'android' && body.version === '1.2.0') return res.json({ status: '200' });
+        if (body.platform === 'android' && body.version === '1.2.1') return res.json({ status: '200' });
 
-        if (body.platform === 'android' && body.version !== '1.2.0') {
+        if (body.platform === 'android' && body.version !== '1.2.1') {
             return res.json({
                 status: '404',
                 msg: `A new update is now available. kindly update your App to get the best experience`
