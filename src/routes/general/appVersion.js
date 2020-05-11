6702683128638017bfd2031710cf605bebb1cb4c
Abdul Haseeb <Haseeb@healrthrix.com>
2020-05-11 12:55:11 +0500

    update status code

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -9,16 +9,16 @@ router.post('/checkVersion', async (req, res) => {
 
         if (body.platform === 'ios' && body.version !== '1.2.1') {
             return res.json({
-                status: '404',
+                status: '200',
                 msg: `A new update is now available. kindly update your App to get the best experience`
             });
         }
 
-        if (body.platform === 'android' && body.version === '1.2.0') return res.json({ status: '200' });
+        if (body.platform === 'android' && body.version === '1.2.1') return res.json({ status: '200' });
 
-        if (body.platform === 'android' && body.version !== '1.2.0') {
+        if (body.platform === 'android' && body.version !== '1.2.1') {
             return res.json({
-                status: '404',
+                status: '200',
                 msg: `A new update is now available. kindly update your App to get the best experience`
             });
         }
