43fd1ed4c56ac038049417efd5f3055cf1f18d79
Umar9292 <umarpromd@gmail.com>
2020-05-11 17:56:23 +0500

    updated checkTIme Api's condition

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -59,8 +59,11 @@ router.post('/checkTime', async (req, res) => {
         const orderTime = moment(formatedOrderTime, 'HH:mma');
         const openingTime = moment(shop.openingTime, 'HH:mma').tz('Asia/karachi');
         const closingTime = moment(shop.closingTime, 'HH:mma').tz('Asia/karachi');
-
-        if (orderTime.isAfter(openingTime) || orderTime.isBefore(closingTime)) {
+        if (
+            orderTime.isAfter(openingTime) &&
+            orderTime.isBefore(closingTime) &&
+            !orderTime.isAfter(closingTime)
+        ) {
             return res.json({ status: '200' });
         }
 
