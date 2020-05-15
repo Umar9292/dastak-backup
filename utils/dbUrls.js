f06a87f13c3cff357a1c2ffec5c0c84d8c57fb85
Abdul Haseeb <Haseeb@healrthrix.com>
2020-05-15 15:42:29 +0500

    change DBurls to developemnt

--- utils/dbUrls.js
+++ utils/dbUrls.js
@@ -1,4 +1,5 @@
 // const baseDbUrl = process.env.DEV_DB;
-let baseDbUrl = process.env.MONGODB_URI;
+//let baseDbUrl = process.env.MONGODB_URI;
+let baseDbUrl = process.env.MONGODB_URI_DEV;
 
 module.exports = baseDbUrl;
