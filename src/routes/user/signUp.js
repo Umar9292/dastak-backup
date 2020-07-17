d8fe97f2b3069f49af05753bb9d5f0ff50f7f258
Umar9292 <umarpromd@gmail.com>
2020-07-17 14:48:26 +0500

    General

--- src/routes/user/signUp.js
+++ src/routes/user/signUp.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
@@ -6,80 +7,100 @@ const _ = require('lodash');
 const User = require('../../models/userModel');
 
 router.post('/signUp', async (req, res) => {
-    try {
-        const params = req.body;
-        let { phone, password } = params;
+  try {
+    const params = req.body;
+    const { phone, password } = params;
 
-        const user = await User.findOne({ phone: phone });
-        if (user) return res.json({
-            status: '404',
-            msg: `The number you have entered is already associated with another account`
-        });
+    const user = await User.findOne({ phone });
+    if (user)
+      return res.json({
+        status: '404',
+        msg: `The number you have entered is already associated with another account`,
+      });
 
-        params.password = await bcrypt.hash(password, 10);
+    params.password = await bcrypt.hash(password, 10);
 
-        const newUser = await new User(params).save();
+    const newUser = await new User(params).save();
 
-        return res.json({
-            status: '200',
-            data: (_.pick(newUser, [
-                '_id', 'phone', 'name', 'image', 'address', 'type'
-            ]))
-        });
-    }
-    catch (err) {
-        return res.json({
-            status: '404',
-            msg: `Looks like something went wrong on our side. Sorry for the inconvenience`,
-            error: err.toString()
-        });
-    }
+    return res.json({
+      status: '200',
+      data: _.pick(newUser, [
+        '_id',
+        'phone',
+        'name',
+        'image',
+        'address',
+        'type',
+      ]),
+    });
+  } catch (err) {
+    return res.json({
+      status: '404',
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience`,
+      error: err.toString(),
+    });
+  }
 });
 
 router.post('/signIn', async (req, res) => {
-    try {
-        let { phone, password } = req.body;
+  try {
+    const { phone, password } = req.body;
 
-        const user = await User.findOne({ phone: phone });
-        if (!user) return res.json({
-            status: '404',
-            msg: `The number you have entered is not associated with any account`
-        });
+    const user = await User.findOne({ phone });
+    if (!user)
+      return res.json({
+        status: '404',
+        msg: `The number you have entered is not associated with any account`,
+      });
 
-        if (user.status === 'inactive') return res.json({
-            status: '404',
-            msg: `You account has been deactivated`
-        });
+    if (user.status === 'inactive')
+      return res.json({
+        status: '404',
+        msg: `You account has been deactivated`,
+      });
 
-        const result = await bcrypt.compare(password, user.password);
-        if (!result) return res.json({
-            status: '404',
-            msg: `Number or password is invalid`
-        });
+    const result = await bcrypt.compare(password, user.password);
+    if (!result)
+      return res.json({
+        status: '404',
+        msg: `Number or password is invalid`,
+      });
 
-        if (user.type === 'user') {
-            return res.json({
-                status: '200',
-                data: (_.pick(user, [
-                    '_id', 'phone', 'name', 'address', 'type', 'playerId'
-                ]))
-            });
-        }
-
-        return res.json({
-            status: '200',
-            data: (_.pick(user, [
-                '_id', 'phone', 'name', 'address', 'type', 'playerId', 'img', 'openingTime', 'closingTime'
-            ]))
-        });
+    if (user.type === 'user') {
+      return res.json({
+        status: '200',
+        data: _.pick(user, [
+          '_id',
+          'phone',
+          'name',
+          'address',
+          'type',
+          'playerId',
+        ]),
+      });
     }
-    catch (err) {
-        res.json({
-            status: '404',
-            msg: `Looks like something went wrong on our side. Sorry for the inconvenience`,
-            error: err.toString()
-        });
-    }
+
+    return res.json({
+      status: '200',
+      data: _.pick(user, [
+        '_id',
+        'phone',
+        'name',
+        'address',
+        'type',
+        'playerId',
+        'img',
+        'openingTime',
+        'closingTime',
+      ]),
+    });
+  } catch (err) {
+    res.json({
+      status: '404',
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience`,
+      error: err.toString(),
+    });
+  }
 });
 
 module.exports = router;
