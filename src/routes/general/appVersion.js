0075e32a40c5f8864d2f8daa55c45dd9cfc456f7
Umar9292 <umarpromd@gmail.com>
2020-10-06 16:30:25 +0500

    updated app versions and changed urls to production.

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -6,37 +6,37 @@ router.post('/checkVersion', async (req, res) => {
   try {
     const { body } = req;
 
-    if (body.platform === 'ios' && body.version === '1.3.7')
+    if (body.platform === 'ios' && body.version === '1.3.8')
       return res.json({ status: '200' });
 
-    /* if (body.platform === 'ios' && body.version === '1.3.6') {
+    if (body.platform === 'ios' && body.version !== '1.3.8') {
       return res.json({
         msg: `Our app is being updated to make the experience even better for you. Kindly try again in 24 hours.`,
       });
-    } */
+    }
 
-    if (body.platform === 'ios' && body.version !== '1.3.7') {
+    /*  if (body.platform === 'ios' && body.version !== '1.3.7') {
       return res.json({
         status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
-    }
+    } */
 
-    if (body.platform === 'android' && body.version === '1.3.7')
+    if (body.platform === 'android' && body.version === '1.3.8')
       return res.json({ status: '200' });
 
-    /* if (body.platform === 'android' && body.version === '1.3.6') {
+    if (body.platform === 'android' && body.version !== '1.3.8') {
       return res.json({
         msg: `Our app is being updated to make the experience even better for you. Kindly try again in 24 hours.`,
       });
-    } */
+    }
 
-    if (body.platform === 'android' && body.version !== '1.3.7') {
+    /*   if (body.platform === 'android' && body.version !== '1.3.7') {
       return res.json({
         status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
-    }
+    } */
   } catch (err) {
     return res.json({
       status: '404',
--- utils/onSignalConfig.js
+++ utils/onSignalConfig.js
@@ -1,10 +1,10 @@
 const OneSignal = require('onesignal-node');
 
 exports.oneSignalClient = new OneSignal.Client({
-    userAuthKey: process.env.ONE_SIGNAL_USER_AUTH_KEY,
+  userAuthKey: process.env.ONE_SIGNAL_USER_AUTH_KEY,
 
-    app: {
-        appAuthKey: process.env.ONE_SIGNAL_APP_AUTH_KEY,
-        appId: process.env.ONE_SIGNAL_APP_ID
-    }
+  app: {
+    appAuthKey: process.env.ONE_SIGNAL_APP_AUTH_KEY,
+    appId: process.env.ONE_SIGNAL_APP_ID,
+  },
 });
