3256ec3872d03566968bbe7726c260d99998e139
Umar9292 <umarpromd@gmail.com>
2020-10-29 13:11:06 +0500

    urls do dev

--- utils/dbUrls.js
+++ utils/dbUrls.js
@@ -1,5 +1,5 @@
 // const baseDbUrl = process.env.DEV_DB;
-const baseDbUrl = process.env.MONGODB_URI;
-// const baseDbUrl = process.env.MONGODB_URI_DEV;
+// const baseDbUrl = process.env.MONGODB_URI;
+const baseDbUrl = process.env.MONGODB_URI_DEV;
 
 module.exports = baseDbUrl;
