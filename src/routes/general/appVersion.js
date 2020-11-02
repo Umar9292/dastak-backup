16e9bd7458e3892d4c2eff155a053f01d0fda6ae
Umar9292 <umarpromd@gmail.com>
2020-11-02 17:10:56 +0500

    changed app versions

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -6,7 +6,7 @@ router.post('/checkVersion', async (req, res) => {
   try {
     const { body } = req;
 
-    if (body.platform === 'ios' && body.version === '1.4.2')
+    if (body.platform === 'ios' && body.version === '1.4.4')
       return res.json({ status: '200' });
 
     /* if (body.platform === 'ios' && body.version !== '1.4.0') {
@@ -15,14 +15,14 @@ router.post('/checkVersion', async (req, res) => {
       });
     } */
 
-    if (body.platform === 'ios' && body.version !== '1.4.2') {
+    if (body.platform === 'ios' && body.version !== '1.4.4') {
       return res.json({
-        status: '404',
+        status: '200',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
     }
 
-    if (body.platform === 'android' && body.version === '1.4.3')
+    if (body.platform === 'android' && body.version === '1.4.4')
       return res.json({ status: '200' });
 
     /*  if (body.platform === 'android' && body.version !== '1.4.0') {
@@ -31,9 +31,9 @@ router.post('/checkVersion', async (req, res) => {
       });
     } */
 
-    if (body.platform === 'android' && body.version !== '1.4.3') {
+    if (body.platform === 'android' && body.version !== '1.4.4') {
       return res.json({
-        status: '404',
+        status: '200',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
     }
