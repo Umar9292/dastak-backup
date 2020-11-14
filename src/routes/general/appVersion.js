6979b84f2c7086c99090cccbf994aca1f6cc31c0
Abdul Haseeb <abdul@abduls-mbp.home>
2020-11-14 09:45:31 +0500

    change dev to prod

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -17,7 +17,7 @@ router.post('/checkVersion', async (req, res) => {
 
     if (body.platform === 'ios' && body.version !== '1.4.6') {
       return res.json({
-        status: '200',
+        status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
     }
@@ -33,7 +33,7 @@ router.post('/checkVersion', async (req, res) => {
 
     if (body.platform === 'android' && body.version !== '1.4.5') {
       return res.json({
-        status: '200',
+        status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
     }
