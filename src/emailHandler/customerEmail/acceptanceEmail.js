cdf4c992b04cb7722374dee0b92854ba6fc92834
Umar9292 <umarpromd@gmail.com>
2021-01-01 22:25:26 +0500

    sending an email to the user when an order is accepted.

---
+++ src/emailHandler/customerEmail/acceptanceEmail.js
@@ -0,0 +1,29 @@
+import { createTransport } from 'nodemailer';
+
+export const sendAcceptanceEmail = msg => {
+  const transporter = createTransport({
+    host: process.env.MAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER_NAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailOptions = {
+    from: 'no-reply@dastak.store',
+    to: 'support@dastak.store',
+    subject: 'Order Status',
+    text: msg,
+  };
+
+  transporter.sendMail(mailOptions, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
