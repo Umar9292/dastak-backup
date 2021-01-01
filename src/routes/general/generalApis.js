9c3ee328eca0bd1a0ad470d905a17752c9e08352
Umar9292 <umarpromd@gmail.com>
2021-01-01 22:28:56 +0500

    generating dynamic excel sheets

--- src/routes/general/generalApis.js
+++ src/routes/general/generalApis.js
@@ -3,7 +3,7 @@ import moment from 'moment-timezone';
 import Exceljs from 'exceljs';
 
 // import Users from '../../models/userModel';
-import Products from '../../models/productsModel';
+// import Products from '../../models/productsModel';
 import Marts from '../../models/martsModel';
 import Orders from '../../models/ordersModel';
 // import { notifyUser } from '../../notificationHandler/handler';
@@ -133,12 +133,12 @@ router.post('/collections', async (req, res) => {
     const thisWeeksOrders = [];
 
     const orders = await Orders.find({
+      paid: false,
       status: { $in: ['Delivered', 'Rider Picked Up'] },
       orderType: 'Delivery',
     });
 
     startDate = moment(startDate, 'DD-MM-YYYY');
-
     endDate = moment(endDate, 'DD-MM-YYYY');
 
     await Promise.all(
@@ -315,7 +315,7 @@ router.get('/deliveryCharges', async (req, res) => {
 
     await Promise.all(
       allUsers.map(async user => {
-        user.deliveryCharges = '0';
+        user.deliveryCharges = '30';
         await user.save();
       })
     );
@@ -330,7 +330,7 @@ router.get('/deliveryCharges', async (req, res) => {
   }
 });
 
-router.post('/test', async (req, res) => {
+router.post('/dailyRiderCollections', async (req, res) => {
   try {
     const { date } = req.body;
 
@@ -363,9 +363,8 @@ router.post('/test', async (req, res) => {
 
     worksheet.getRow(1).eachCell(cell => (cell.font = { bold: true }));
 
-    const buffer = await workbook.xlsx.writeBuffer();
-
-    sendDailyCollection(date, buffer);
+    await workbook.xlsx.writeFile(`${date}.xlsx`);
+    await sendDailyCollection(`${date}.xlsx`);
 
     const totalCollection = data.reduce((a, b) => a + b.collection, 0);
 
@@ -376,7 +375,222 @@ router.post('/test', async (req, res) => {
       status: '200',
     });
   } catch (err) {
-    console.log(err);
+    return res.json({
+      status: '404',
+      error: err.toString(),
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience.`,
+    });
+  }
+});
+
+router.get('/weeklyRidersFare', async (req, res) => {
+  try {
+    const [startDate, endDate] = ['24-12-2020', '30-12-2020'];
+    let thisWeeksOrders = [];
+    const dateRange = `${startDate} - ${endDate}`;
+
+    const start = moment(startDate, 'DD-MM-YYYY');
+    const end = moment(endDate, 'DD-MM-YYYY');
+
+    const riders = await Orders.distinct('riderName', {
+      date: {
+        $in: [
+          '24-12-2020',
+          '25-12-2020',
+          '26-12-2020',
+          '27-12-2020',
+          '28-12-2020',
+          '29-12-2020',
+          '30-12-2020',
+        ],
+      },
+    });
+
+    const data = await Promise.all(
+      riders.map(async riderName => {
+        const orders = await Orders.find({
+          riderName,
+          paidToRider: { $in: [false, undefined] },
+          orderType: 'Delivery',
+          status: 'Delivered',
+        });
+
+        await Promise.all(
+          orders.map(order => {
+            const orderDate = moment(order.date, 'DD-MM-YYYY');
+
+            if (
+              orderDate.isSameOrAfter(start) &&
+              orderDate.isSameOrBefore(end)
+            ) {
+              thisWeeksOrders.push(order);
+            }
+          })
+        );
+
+        const total = thisWeeksOrders.reduce((a, b) => a + b.orderTotal, 0);
+        const riderFare = thisWeeksOrders.reduce((a, b) => a + b.riderFare, 0);
+
+        const result = {
+          dateRange,
+          riderName,
+          total,
+          riderFare,
+          orders: thisWeeksOrders.length,
+        };
+
+        thisWeeksOrders = [];
+        return result;
+      })
+    );
+
+    const workbook = new Exceljs.Workbook();
+    const worksheet = workbook.addWorksheet(dateRange);
+
+    worksheet.columns = [
+      { header: 'Date Range', key: 'dateRange', width: 15 },
+      { header: 'Rider Name', key: 'riderName', width: 15 },
+      { header: 'Total Collection', key: 'total', width: 15 },
+      { header: 'Rider Fare', key: 'riderFare', width: 15 },
+    ];
+
+    await Promise.all(data.map(doc => worksheet.addRow(doc)));
+
+    worksheet.getRow(1).eachCell(cell => (cell.font = { bold: true }));
+
+    await workbook.xlsx.writeFile(`${dateRange}.xlsx`);
+    await sendDailyCollection(`${dateRange}.xlsx`);
+
+    return res.json({ status: '200', data });
+  } catch (err) {
+    return res.json({
+      status: '404',
+      error: err.toString(),
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience.`,
+    });
+  }
+});
+
+router.post('/restaurantsCollections', async (req, res) => {
+  try {
+    const { startDate, endDate } = req.body;
+    let thisWeeksOrders = [];
+    let percentage = 0;
+    let dateRange;
+
+    const start = moment(startDate, 'DD-MM-YYYY');
+    const end = moment(endDate, 'DD-MM-YYYY');
+
+    const restaurants = await Orders.distinct('martName', {
+      date: {
+        $in: [
+          '24-12-2020',
+          '25-12-2020',
+          '26-12-2020',
+          '27-12-2020',
+          '28-12-2020',
+          '29-12-2020',
+          '30-12-2020',
+        ],
+      },
+    });
+
+    const data = await Promise.all(
+      restaurants.map(async martName => {
+        if (
+          martName === 'Zam Zam Restaurant' ||
+          martName === 'De Fiesta Restaurant'
+        ) {
+          percentage = 10;
+        } else if (martName === "Moody's") {
+          percentage = 12;
+        } else {
+          percentage = 15;
+        }
+
+        const orders = await Orders.find({
+          martName,
+          paid: { $in: [false, undefined] },
+          orderType: 'Delivery',
+          status: 'Delivered',
+        });
+
+        await Promise.all(
+          orders.map(order => {
+            const orderDate = moment(order.date, 'DD-MM-YYYY');
+
+            if (
+              orderDate.isSameOrAfter(start) &&
+              orderDate.isSameOrBefore(end)
+            ) {
+              thisWeeksOrders.push(order);
+            }
+          })
+        );
+
+        const total = thisWeeksOrders.reduce((a, b) => a + b.orderTotal, 0);
+        const withoutDelivery = thisWeeksOrders.reduce(
+          (a, b) =>
+            b.deliveryCharges !== '0'
+              ? a + b.orderTotal - 30
+              : a + b.orderTotal,
+          0
+        );
+        const ourProfit = ((percentage / 100) * withoutDelivery).toFixed();
+        const totalDeliveryCharges = total - withoutDelivery;
+        const totalToPayOwner = withoutDelivery - +ourProfit;
+        dateRange = `${startDate} - ${endDate}`;
+
+        const result = {
+          dateRange,
+          martName,
+          total,
+          totalToPayOwner,
+          ourProfit,
+          totalDeliveryCharges,
+          totalOrders: thisWeeksOrders,
+        };
+
+        thisWeeksOrders = [];
+        return result;
+      })
+    );
+
+    const [totalAmount, amountToPay] = [
+      data.reduce((a, b) => a + b.total, 0),
+      data.reduce((a, b) => a + b.totalToPayOwner, 0),
+    ];
+
+    await Promise.all(
+      data.map(doc => {
+        doc.totalAmount = totalAmount;
+        doc.amountToPay = amountToPay;
+      })
+    );
+
+    const workbook = new Exceljs.Workbook();
+    const worksheet = workbook.addWorksheet(dateRange);
+
+    worksheet.columns = [
+      { header: 'Date Range', key: 'dateRange', width: 15 },
+      { header: 'Total Amount', key: 'total', width: 15 },
+      { header: 'Restaurant Name', key: 'martName', width: 15 },
+      { header: 'After Percentage', key: 'totalToPayOwner', width: 15 },
+      { header: 'Profit', key: 'ourProfit', width: 15 },
+      { header: 'Delivery Charges', key: 'totalDeliveryCharges', width: 15 },
+      { header: 'Total Amount', key: 'totalAmount', width: 15 },
+      { header: 'Total Amount to Pay', key: 'amountToPay', width: 15 },
+    ];
+
+    await Promise.all(data.map(doc => worksheet.addRow(doc)));
+
+    worksheet.getRow(1).eachCell(cell => (cell.font = { bold: true }));
+
+    await workbook.xlsx.writeFile(`${dateRange}.xlsx`);
+    await sendDailyCollection(`${dateRange}.xlsx`);
+
+    return res.json({ status: '200', data });
+  } catch (err) {
     return res.json({
       status: '404',
       error: err.toString(),
