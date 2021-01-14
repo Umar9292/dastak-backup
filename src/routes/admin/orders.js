ea311cc1291c3cfc4d85a3d0a05ba0270325bda3
Umar9292 <umarpromd@gmail.com>
2021-01-14 12:47:59 +0500

    general updates

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -733,7 +733,7 @@ router.post('/paidToRiders', async (req, res) => {
 
           Orders.find({
             riderId,
-            paidToRider: false,
+            paidToRider: true,
             orderType: 'Delivery',
             status: { $in: ['Delivered', 'Rider Picked Up'] },
             dateForSearching: {
