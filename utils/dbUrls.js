11b06d3aa22909731f6352539d5e27466237ad45
Umar9292 <umarpromd@gmail.com>
2021-01-15 20:14:22 +0500

    urls to dev

--- utils/dbUrls.js
+++ utils/dbUrls.js
@@ -1,3 +1,3 @@
 // const baseDbUrl = process.env.DEV_DB;
-export const dbUrl = process.env.MONGODB_URI;
-// export const dbUrl = process.env.MONGODB_URI_DEV;
+// export const dbUrl = process.env.MONGODB_URI;
+export const dbUrl = process.env.MONGODB_URI_DEV;
