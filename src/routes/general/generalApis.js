c352d510fce51cd087246609d8a079c13686c990
Umar9292 <umarpromd@gmail.com>
2021-01-15 15:42:28 +0500

    bug fixes

--- src/routes/general/generalApis.js
+++ src/routes/general/generalApis.js
@@ -547,8 +547,8 @@ router.post('/restaurantsCollections', async (req, res) => {
 
     worksheet.getRow(1).eachCell(cell => (cell.font = { bold: true }));
 
-    // await workbook.xlsx.writeFile(`${dateRange}.xlsx`);
-    // sendDailyCollection(`${dateRange}.xlsx`);
+    await workbook.xlsx.writeFile(`${dateRange}.xlsx`);
+    sendDailyCollection(`${dateRange}.xlsx`);
 
     return res.json({ status: '200', data });
   } catch (err) {
