e1c8573c179119feba4eae076ca35ef25cb762b6
Umar9292 <umarpromd@gmail.com>
2021-01-18 18:00:13 +0500

    testing updated rider fares scenario

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -54,28 +54,6 @@ router.post('/placeOrder', async (req, res) => {
         .toISOString(),
     };
 
-    const morningFareTime = moment('04:00', 'HH:mm:ssa').tz('Asia/karachi');
-    const noonFareTime = moment('16:00', 'HH:mm:ssa').tz('Asia/karachi');
-    const nightFareEndTime = moment('18:59', 'HH:mm:ssa').tz('Asia/karachi');
-
-    if (
-      orderTime.isBetween(
-        `${morningFareTime.toISOString()}`,
-        `${noonFareTime.toISOString()}`
-      )
-    ) {
-      params.riderFare = 60;
-    }
-
-    if (
-      orderTime.isBetween(
-        `${noonFareTime.toISOString()}`,
-        `${nightFareEndTime.toISOString()}`
-      )
-    ) {
-      params.riderFare = 80;
-    }
-
     const order = await new Orders(params).save();
 
     const adminMessage = 'You have a new order';
@@ -465,7 +443,10 @@ router.post('/assignRider', async (req, res) => {
   try {
     const { orderId, riderName, riderId } = req.body;
 
-    const order = await Orders.findById(orderId);
+    const [order, { tillNoonFare, nightFare }] = await Promise.all([
+      Orders.findById(orderId),
+      Users.findById(riderId),
+    ]);
 
     if (order.riderId)
       return res.json({
@@ -474,9 +455,32 @@ router.post('/assignRider', async (req, res) => {
           'This order has already been assigned to another rider. Stay active another order might come your way.',
       });
 
+    const orderTime = moment(order.time, 'HH:mm').tz('Asia/karachi');
+    console.log(orderTime);
+    const morningFareTime = moment('04:00', 'HH:mm:ssa').tz('Asia/karachi');
+    const noonFareTime = moment('16:00', 'HH:mm:ssa').tz('Asia/karachi');
+    const nightFareEndTime = moment('18:59', 'HH:mm:ssa').tz('Asia/karachi');
+
+    if (
+      orderTime.isBetween(
+        `${morningFareTime.toISOString()}`,
+        `${noonFareTime.toISOString()}`
+      )
+    ) {
+      req.body.riderFare = tillNoonFare;
+    }
+
+    if (
+      orderTime.isBetween(
+        `${noonFareTime.toISOString()}`,
+        `${nightFareEndTime.toISOString()}`
+      )
+    ) {
+      req.body.riderFare = nightFare;
+    }
+
     await Promise.all([
       Orders.findByIdAndUpdate(orderId, { $set: req.body }),
-
       Users.findByIdAndUpdate(riderId, {
         status: 'on delivery',
       }),
