4eaabd388f990ad7439131c0db64551ee462c563
Umar9292 <umarpromd@gmail.com>
2021-01-21 19:23:47 +0500

    added an ai to reassign rider

--- src/routes/admin/dashboard/riderManagement.js
+++ src/routes/admin/dashboard/riderManagement.js
@@ -1,6 +1,7 @@
 const Router = require('express/lib/router');
 
 const Users = require('../../../models/userModel');
+const Orders = require('../../../models/ordersModel');
 
 const router = Router();
 
@@ -12,6 +13,22 @@ router.get('/activeRiders', async (_req, res) => {
     });
 
     return res.json({ status: '200', activeRiders });
+  } catch (err) {
+    return res.json({
+      status: '404',
+      error: err.toString(),
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience.`,
+    });
+  }
+});
+
+router.post('/reAssignRider', async (req, res) => {
+  try {
+    const { orderId } = req.body;
+
+    await Orders.findByIdAndUpdate(orderId, { $set: req.body });
+
+    return res.json({ status: '200', msg: 'This order has been re assigned' });
   } catch (err) {
     return res.json({
       status: '404',
