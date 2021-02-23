ae25c022d5a49837a62c475f3847e7925c8b3232
Umar9292 <umarpromd@gmail.com>
2021-02-23 13:40:33 +0500

    updated app versions api

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -17,7 +17,7 @@ router.post('/checkVersion', async (req, res) => {
 
     if (platform === 'ios' && version !== '1.5.5') {
       return res.json({
-        status: '200',
+        status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
     }
@@ -33,7 +33,7 @@ router.post('/checkVersion', async (req, res) => {
 
     if (platform === 'android' && version !== '1.5.4') {
       return res.json({
-        status: '200',
+        status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
       });
     }
