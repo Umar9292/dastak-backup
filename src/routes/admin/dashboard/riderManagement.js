8bc745878b732de5bf9e9cab157dea2d0ec3210a
Umar9292 <umarpromd@gmail.com>
2021-03-01 17:23:20 +0500

    fixed a bug in rider management and urls to prod

--- src/routes/admin/dashboard/riderManagement.js
+++ src/routes/admin/dashboard/riderManagement.js
@@ -213,7 +213,7 @@ router.post('/ridersFare', async (req, res) => {
     );
 
     const riderEarnings = data.reduce((a, b) => a + b.riderFare, 0);
-    const totalCollection = data.reduce((a, b) => a + b.orderTotal, 0);
+    const totalCollection = data.reduce((a, b) => a + b.collection, 0);
 
     return res.json({ status: '200', data, riderEarnings, totalCollection });
   } catch (err) {
