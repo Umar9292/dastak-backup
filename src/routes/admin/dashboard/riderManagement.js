127ef1245fd383c18ffbf4bc883d62a1e52bbb97
Umar9292 <umarpromd@gmail.com>
2021-03-22 12:28:21 +0500

    testing riders fare

--- src/routes/admin/dashboard/riderManagement.js
+++ src/routes/admin/dashboard/riderManagement.js
@@ -211,6 +211,8 @@ router.post('/ridersFare', async (req, res) => {
       })
     );
 
+    console.log(data.length);
+
     const riderEarnings = data.reduce((a, b) => a + b.riderFare, 0);
     const totalCollection = data.reduce((a, b) => a + b.collection, 0);
 
