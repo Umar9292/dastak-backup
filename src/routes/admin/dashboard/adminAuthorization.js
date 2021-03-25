1e0d028e36f28a58091770b0803d4d46cda2d5d9
Umar9292 <umarpromd@gmail.com>
2021-03-25 14:15:52 +0500

    addded an api for admin sign in an d admin authorization.

---
+++ src/routes/admin/dashboard/adminAuthorization.js
@@ -0,0 +1,29 @@
+const Router = require('express/lib/router');
+
+const User = require('../../../models/userModel');
+
+const router = Router();
+
+router.post('/authorization', async (req, res) => {
+  try {
+    const { adminId } = req.body;
+
+    const user = await User.findById({ _id: adminId }).lean();
+    if (!user || !user.superAdmin) {
+      return res.json({
+        status: '404',
+        msg: `You are not authorized.`,
+      });
+    }
+
+    return res.json({ status: '200' });
+  } catch (err) {
+    res.json({
+      status: '404',
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience`,
+      error: err.toString(),
+    });
+  }
+});
+
+module.exports = router;
---
+++ src/routes/admin/dashboard/adminSignIn.js
@@ -0,0 +1,43 @@
+const Router = require('express/lib/router');
+const { compare } = require('bcrypt');
+
+const User = require('../../../models/userModel');
+
+const router = Router();
+
+router.post('/signIn', async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+
+    const user = await User.findOne({ phone }).lean();
+    if (!user || !user.superAdmin) {
+      return res.json({
+        status: '404',
+        msg: `You are not authorized.`,
+      });
+    }
+
+    const result = await compare(password, user.password);
+    if (!result) {
+      return res.json({
+        status: '404',
+        msg: `Number or password is invalid`,
+      });
+    }
+
+    user.password = null;
+
+    return res.json({
+      status: '200',
+      data: user,
+    });
+  } catch (err) {
+    res.json({
+      status: '404',
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience`,
+      error: err.toString(),
+    });
+  }
+});
+
+module.exports = router;
