b73b95213d975d67744aaf81afc4d8500c53f8d9
Umar9292 <umarpromd@gmail.com>
2021-03-30 13:51:22 +0500

    added rider address field to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -24,6 +24,7 @@ const userModel = Schema(
       type: String,
       default: 'active',
     },
+    riderAddress: String,
     employee: Boolean,
     available: Boolean,
     jazzCashNumber: String,
