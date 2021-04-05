6a60673549d4ad133ee2e99aa32db08dd1a0bbbc
Umar9292 <umarpromd@gmail.com>
2021-04-05 16:49:58 +0500

    testing

--- src/routes/general/generalApis.js
+++ src/routes/general/generalApis.js
@@ -790,6 +790,8 @@ router.post('/dealMoney', async (req, res) => {
         })
       );
 
+      console.log(data);
+
       const workbook = new Exceljs.Workbook();
       const worksheet = workbook.addWorksheet(`${startDate - endDate}`);
 
