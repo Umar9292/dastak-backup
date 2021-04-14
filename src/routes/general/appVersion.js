99425dcc865bfe936fcc4e671420ae7efb39380f
Umar9292 <umarpromd@gmail.com>
2021-04-14 13:14:01 +0500

    updated app versions

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -6,7 +6,7 @@ router.post('/checkVersion', async (req, res) => {
   try {
     const { platform, version } = req.body;
 
-    if (platform === 'ios' && (version === '1.5.8' || version === '1.5.9')) {
+    if (platform === 'ios' && version === '1.5.9') {
       return res.json({ status: '200', showCategories: 'false' });
     }
 
@@ -16,7 +16,7 @@ router.post('/checkVersion', async (req, res) => {
       });
     } */
 
-    if (platform === 'ios' && (version !== '1.5.8' || version !== '1.5.9')) {
+    if (platform === 'ios' && version !== '1.5.9') {
       return res.json({
         status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
@@ -24,10 +24,7 @@ router.post('/checkVersion', async (req, res) => {
       });
     }
 
-    if (
-      platform === 'android' &&
-      (version === '1.5.8' || version === '1.5.9')
-    ) {
+    if (platform === 'android' && version === '1.5.9') {
       return res.json({ status: '200', showCategories: 'false' });
     }
 
@@ -37,10 +34,7 @@ router.post('/checkVersion', async (req, res) => {
       });
     } */
 
-    if (
-      platform === 'android' &&
-      (version !== '1.5.8' || version !== '1.5.9')
-    ) {
+    if (platform === 'android' && version !== '1.5.9') {
       return res.json({
         status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
