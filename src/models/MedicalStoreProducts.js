29cbb9a684385467273f27735ad69e66b785a075
Umar9292 <umarpromd@gmail.com>
2021-04-26 18:27:03 +0500

    added an api for all medical stores. created a schema for medical store products.

---
+++ src/models/MedicalStoreProducts.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const medicalStoreProducts = Schema(
+  {
+    martId: String,
+    available: {
+      default: 'in stock',
+      type: String,
+    },
+    imgUrl: String,
+    productName: String,
+    price: Number,
+    category: String,
+    quantity: String,
+    net: Number,
+    count: Number,
+    type: String,
+    discount: String,
+    discountedPrice: Number,
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = model('products', medicalStoreProducts);
