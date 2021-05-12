c33d70900936d7f20331eba7ad23e6274e3ccda2
Umar9292 <umarpromd@gmail.com>
2021-05-12 14:00:58 +0500

    updated assign rider scenario

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -612,7 +612,7 @@ router.post('/adminAcceptedOrders', async (req, res) => {
   }
 });
 
-/* router.post('/assignRider', async (req, res) => {
+router.post('/assignRider', async (req, res) => {
   try {
     const { orderId, riderName, riderId, admin } = req.body;
 
@@ -797,9 +797,9 @@ router.post('/adminAcceptedOrders', async (req, res) => {
       msg: `Looks like something went wrong on our side. Sorry for the inconvenience.`,
     });
   }
-}); */
+});
 
-router.post('/assignRider', async (req, res) => {
+/* router.post('/assignRider', async (req, res) => {
   try {
     const { orderId, riderName, riderId, admin } = req.body;
 
@@ -913,7 +913,7 @@ router.post('/assignRider', async (req, res) => {
     });
   }
 });
-
+ */
 router.post('/riderOrders', async (req, res) => {
   try {
     const { riderId } = req.body;
