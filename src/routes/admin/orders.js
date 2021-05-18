518bf34f44fa46f451e1016bf51880116b3a3b52
Umar9292 <umarpromd@gmail.com>
2021-05-19 00:36:07 +0500

    testing

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -632,10 +632,8 @@ router.post('/assignRider', async (req, res) => {
 
     console.log(time);
 
-    const depositTimeUpperLimit = moment('04:00', 'HH:mm').tz('Asia/Karachi');
-    const depositTimeLowerLimit = moment('03:00', 'HH:mm')
-      .tz('Asia/Karachi')
-      .subtract(5, 'hours');
+    const depositTimeUpperLimit = moment('09:00', 'HH:mm');
+    const depositTimeLowerLimit = moment('03:00', 'HH:mm');
 
     console.log(depositTimeLowerLimit);
     console.log(depositTimeUpperLimit);
