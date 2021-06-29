8758e9f01c20bb578074c0db8653710b75f2d5e2
Umar9292 <umarpromd@gmail.com>
2021-06-29 17:10:42 +0500

    reduced featured restaurant distance to 4k

--- src/routes/marts/marts.js
+++ src/routes/marts/marts.js
@@ -107,7 +107,7 @@ router.post('/allRestaurants', async (req, res) => {
           $geoNear: {
             near: { type: 'Point', coordinates: [long, lat] },
             distanceField: 'dist',
-            maxDistance: 5000,
+            maxDistance: 4000,
             query: {
               available: true,
               status: 'active',
