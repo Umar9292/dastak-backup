51af6d8d29349dc737af87ec69560d0719207d5c
Umar9292 <umarpromd@gmail.com>
2021-07-05 15:25:32 +0500

    minor updates

--- src/routes/admin/dashboard/restaurantManagement.js
+++ src/routes/admin/dashboard/restaurantManagement.js
@@ -105,7 +105,7 @@ router.post('/restaurantCollections', async (req, res) => {
           +((percentage / 100) * totalWithoutDelivery).toFixed() +
           deliveryCharges;
 
-        console.log(restaurant, ourProfit);
+        console.log(restaurant, totalWithoutDelivery, ourProfit);
 
         const totalToPay =
           totalOfDeliveryOrders > 0 ? totalOfDeliveryOrders - ourProfit : 0;
