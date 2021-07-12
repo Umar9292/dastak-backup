3e4df860c4eeb2a5b1cf2cd9204dc2dd1c16d91f
Umar9292 <umarpromd@gmail.com>
2021-07-12 18:12:13 +0500

    general

--- src/routes/admin/dashboard/restaurantManagement.js
+++ src/routes/admin/dashboard/restaurantManagement.js
@@ -177,14 +177,14 @@ router.post('/paidToOwners', async (req, res) => {
           Orders.updateMany(
             {
               martId: id,
-              paid: true,
+              paid: false,
               status: { $in: ['Delivered', 'Rider Picked Up'] },
               dateForSearching: {
                 $gte: startDate,
                 $lte: endDate,
               },
             },
-            { paid: false }
+            { paid: true }
           ),
         ]);
       })
