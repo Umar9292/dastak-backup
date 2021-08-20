d1db047702bc8173812301755f96ea89f5a96919
Umar9292 <umarpromd@gmail.com>
2021-08-20 16:17:12 +0500

    added an api for rejected orders

--- src/routes/marts/marts.js
+++ src/routes/marts/marts.js
@@ -374,4 +374,34 @@ router.post('/previousPayments', async (req, res) => {
   }
 });
 
+router.post('/rejectedOrders', async (req, res) => {
+  try {
+    const { martId, startDate, endDate } = req.body;
+
+    const start = moment(startDate, 'DD-MM-YYYY')
+      .tz('Asia/Karachi')
+      .toISOString();
+    const end = moment(endDate, 'DD-MM-YYYY')
+      .tz('Asia/Karachi')
+      .toISOString();
+
+    const rejectedOrders = await Orders.find({
+      martId,
+      status: 'Rejected',
+      dateForSearching: { $gte: start, $lte: end },
+    })
+      .sort({ createdAt: -1 })
+      .lean();
+
+    return res.json({ status: '200', rejectedOrders });
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      status: '404',
+      error: err.toString(),
+      msg: `Looks like something went wrong on our side. Sorry for the inconvenience.`,
+    });
+  }
+});
+
 module.exports = router;
