0b864c758a122a8489847978be947b20c5cd1fda
Umar9292 <umarpromd@gmail.com>
2021-08-22 02:25:14 +0500

    testing

--- src/routes/general/generalApis.js
+++ src/routes/general/generalApis.js
@@ -345,9 +345,8 @@ router.post('/addActualPrices', async (req, res) => {
 
                   if (product.actualPrice !== undefined) {
                     p.actualPrice = product.actualPrice;
+                    await order.save();
                   }
-
-                  await order.save();
                 }
               })
             );
