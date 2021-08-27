95d986d3fdfd05e2ed324b9928d4b53cd4ce380a
Umar9292 <umarpromd@gmail.com>
2021-08-27 13:27:59 +0500

    updated app versions

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -6,7 +6,7 @@ router.post('/checkVersion', async (req, res) => {
   try {
     const { platform, version } = req.body;
 
-    if (platform === 'ios' && (version === '1.6.3' || version === '1.6.7')) {
+    if (platform === 'ios' && version === '1.6.7') {
       return res.json({ status: '200', showCategories: 'false' });
     }
 
@@ -16,9 +16,9 @@ router.post('/checkVersion', async (req, res) => {
       });
     } */
 
-    if (platform === 'ios' && (version === '1.6.3' || version === '1.6.7')) {
+    if (platform === 'ios' && version === '1.6.7') {
       return res.json({
-        status: '200',
+        status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience`,
         showCategories: 'false',
       });
