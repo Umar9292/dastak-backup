6595d8e6119fe0c3309d1bf79f59e1abba3017ed
Umar9292 <umarpromd@gmail.com>
2021-09-01 17:13:30 +0500

    testing dastak deals and store products

--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -546,6 +546,36 @@ router.post('/dastakDeals', async (req, res) => {
           Flavours.findOne({ martId }),
         ]);
 
+        const [longitude, latitude] = restaurant.geometry.coordinates;
+        const { data: distanceData } = await axios.get(
+          `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${lat},${long}&destinations=${latitude},${longitude}&key=${process.env.GOOGLE_API_KEY}`
+        );
+
+        const distance = distanceData.rows[0].elements[0].distance.text.substring(
+          0,
+          3
+        );
+
+        let deliveryCharges = 0;
+
+        if (+distance <= 1) {
+          deliveryCharges = 20;
+        }
+
+        if (+distance > 1 && +distance <= 2) {
+          deliveryCharges = 30;
+        }
+
+        if (+distance > 2 && +distance <= 4) {
+          deliveryCharges = 40;
+        }
+
+        if (+distance > 4) {
+          deliveryCharges = 50;
+        }
+
+        restaurant.deliveryCharges = deliveryCharges;
+
         if (products.length > 0) {
           for (const product of products) {
             const { regular, drinks, type } = product;
