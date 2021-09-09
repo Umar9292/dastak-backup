d6d578c52c36704cd004866add3dfb20dde3bbbe
Umar9292 <umarpromd@gmail.com>
2021-09-09 14:00:46 +0500

    applied percentage on pickup orders as well.

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -270,6 +270,15 @@ router.post('/allOrders', async (req, res) => {
       ({ orderType }) => orderType === 'Delivery'
     );
 
+    const pickupOrders = delivered.filter(
+      ({ orderType }) => orderType === 'pickUp'
+    );
+
+    const pickupOrdersTotal = pickupOrders.reduce(
+      (a, b) => a + b.orderTotal,
+      0
+    );
+
     await Promise.all(
       deliveryOrders.map(async ({ products }) => {
         await Promise.all(
@@ -293,8 +302,19 @@ router.post('/allOrders', async (req, res) => {
       })
     );
 
-    const ourPercentage = +((percentage / 100) * nonDealPayment).toFixed();
-    const totalToPay = dealPayment + (nonDealPayment - ourPercentage);
+    const deliveryOrdersPercentage = +(
+      (percentage / 100) *
+      nonDealPayment
+    ).toFixed();
+
+    const pickupOrdersPercentage = +(
+      (percentage / 100) *
+      pickupOrdersTotal
+    ).toFixed();
+
+    const totalPercentage = deliveryOrdersPercentage + pickupOrdersPercentage;
+
+    const totalToPay = dealPayment + (nonDealPayment - totalPercentage);
 
     return res.json({
       status: '200',
