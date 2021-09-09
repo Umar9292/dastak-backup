b761106bb04a731b54fda62669c93dd74111e8e1
Umar9292 <umarpromd@gmail.com>
2021-09-09 16:16:49 +0500

    testing reataurant collections

--- src/routes/admin/dashboard/restaurantManagement.js
+++ src/routes/admin/dashboard/restaurantManagement.js
@@ -151,7 +151,8 @@ router.post('/restaurantCollections', async (req, res) => {
         );
 
         const ourPercentage = +((percentage / 100) * nonDealPayment).toFixed();
-        const totalToPay = dealPayment + (nonDealPayment - ourPercentage);
+        const totalToPay =
+          dealPayment + (nonDealPayment - ourPercentage - ourProfit);
         ourProfit += ourPercentage + deliveryCharges;
 
         return {
