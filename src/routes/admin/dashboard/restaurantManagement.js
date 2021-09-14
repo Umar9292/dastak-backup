f9927969d6271ef6b71e4f105b86f47919bc0819
Umar9292 <umarpromd@gmail.com>
2021-09-14 18:28:48 +0500

    added date and time to orders in restaurant collections

--- src/routes/admin/dashboard/restaurantManagement.js
+++ src/routes/admin/dashboard/restaurantManagement.js
@@ -74,7 +74,7 @@ router.post('/restaurantCollections', async (req, res) => {
             },
           })
             .select(
-              'riderFare deliveryCharges martName products orderType orderTotal orderNum'
+              'riderFare deliveryCharges martName products orderType orderTotal orderNum date time'
             )
             .sort({ createdAt: -1 })
             .lean(),
--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -10,6 +10,10 @@ const StoreProducts = require('../../models/storeProducts');
 const Flavours = require('../../models/flavoursAndDrinks');
 const Categories = require('../../models/categoriesModel');
 
+const {
+  calculateDeliveryCharges,
+} = require('../../calculateDealiveryCharges/calculateDeliveryCharges');
+
 const client = createClient(process.env.REDIS_URL);
 
 const router = Router();
@@ -136,34 +140,12 @@ router.post('/allProducts', async (req, res) => {
 
       const restaurant = await Users.findById(martId).lean();
 
-      const { data: distanceData } = await axios.get(
-        `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${+userLatitude},${+userLongitude}&destinations=${+martLatitude},${+martLongitude}&key=${
-          process.env.GOOGLE_API_KEY
-        }`
+      const deliveryCharges = await calculateDeliveryCharges(
+        userLatitude,
+        userLongitude,
+        martLatitude,
+        martLongitude
       );
-
-      const distance = distanceData.rows[0].elements[0].distance.text.substring(
-        0,
-        3
-      );
-
-      let deliveryCharges = 0;
-
-      if (+distance <= 1) {
-        deliveryCharges = 20;
-      }
-
-      if (+distance > 1 && +distance <= 2) {
-        deliveryCharges = 30;
-      }
-
-      if (+distance > 2 && +distance <= 4) {
-        deliveryCharges = 40;
-      }
-
-      if (+distance > 4) {
-        deliveryCharges = 50;
-      }
 
       restaurant.deliveryCharges = deliveryCharges;
 
