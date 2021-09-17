2f85a750f2eb0ce483d55d91bcc290564a992cba
Umar9292 <umarpromd@gmail.com>
2021-09-17 16:07:06 +0500

    testing dastak deals

--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -451,11 +451,14 @@ router.post('/dastakDeals', async (req, res) => {
   try {
     let { lat, long, employee, city } = req.body;
 
+    console.log(req.body);
+
     const currentTime = moment().tz('Asia/Karachi');
 
     let restaurants = [];
 
     if (!employee) {
+      console.log('here');
       restaurants = await Users.aggregate([
         {
           $geoNear: {
