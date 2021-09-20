ad51a72f069a40123fd49b0740e6f3f5f777cde7
Umar9292 <umarpromd@gmail.com>
2021-09-20 16:54:10 +0500

    testing

--- src/routes/admin/dashboard/restaurantManagement.js
+++ src/routes/admin/dashboard/restaurantManagement.js
@@ -424,6 +424,8 @@ router.post('/expensesTillNow', async (req, res) => {
           })
         );
 
+        console.log(martName, ourProfit);
+
         const deliveryOrders = orders.filter(
           ({ orderType }) => orderType === 'Delivery'
         );
