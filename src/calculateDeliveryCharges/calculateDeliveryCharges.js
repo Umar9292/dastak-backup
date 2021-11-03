3ee88427eb1812fb78fd054f772c6667192fc778
Umar9292 <umarpromd@gmail.com>
2021-11-03 15:23:46 +0500

    testing

--- src/calculateDeliveryCharges/calculateDeliveryCharges.js
+++ src/calculateDeliveryCharges/calculateDeliveryCharges.js
@@ -1,27 +1,20 @@
+require('dotenv').config();
 const axios = require('axios');
 
-exports.calculateDeliveryCharges = async (
+const calculateDeliveryCharges = async (
   userLatitude,
   userLongitude,
   martLatitude,
   martLongitude
 ) => {
-  const { data: distanceData } = await axios.get(
-    `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${+userLatitude},${+userLongitude}&destinations=${+martLatitude},${+martLongitude}&key=${
-      process.env.GOOGLE_API_KEY
-    }`
+  const result = await axios.get(
+    `https://us1.locationiq.com/v1/matrix/driving/${userLongitude},${userLatitude};${martLongitude},${martLatitude}?annotations=distance&key=${process.env.LOCATION_IQ_KEY}`
   );
 
-  const distance = distanceData.rows[0].elements[0].distance.text.substring(
-    0,
-    3
-  );
+  const distance = result.data.distances[0][1].toFixed(1);
+  console.log(distance);
 
   let deliveryCharges = 0;
-
-  if (distance.includes('m')) {
-    deliveryCharges = 20;
-  }
 
   if (+distance <= 1) {
     deliveryCharges = 20;
@@ -41,3 +34,10 @@ exports.calculateDeliveryCharges = async (
 
   return deliveryCharges;
 };
+
+calculateDeliveryCharges(
+  32.07546878829667,
+  72.67687864601612,
+  32.03429060235852,
+  72.70534627139568
+);
