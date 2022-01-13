e9008e5c0b5d649df30f9602eafbdb7d73331d43
Umar9292 <umarpromd@gmail.com>
2022-01-13 14:00:04 +0500

    fixed a deal flavours issue in dastak deals.

--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -331,15 +331,19 @@ router.post('/dastakDeals', async (req, res) => {
           for (const product of products) {
             const { regular, drinks, type } = product;
 
-            if (
-              (type === 'deal' && !regular) ||
-              (type === 'deal' && regular === undefined)
-            ) {
-              product.flavours = options.flavours;
-            }
+            if (product.dealFlavours) {
+              product.flavours = options.dealFlavours;
+            } else {
+              if (
+                (type === 'deal' && !regular) ||
+                (type === 'deal' && regular === undefined)
+              ) {
+                product.flavours = options.flavours;
+              }
 
-            if (type === 'deal' && regular === true) {
-              product.flavours = options.regularFlavours;
+              if (type === 'deal' && regular === true) {
+                product.flavours = options.regularFlavours;
+              }
             }
 
             if (drinks === true) {
