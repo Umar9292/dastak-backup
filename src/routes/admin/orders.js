daebcbfc044d52d69fed4bcd53459cafdd6a55f3
Umar9292 <umarpromd@gmail.com>
2022-02-01 13:40:17 +0500

    adjusted riders late night fare timings

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -727,7 +727,7 @@ router.post('/assignRider', async (req, res) => {
     const {
       tillNoonFare,
       nightFare,
-      // lateNightFare,
+      lateNightFare,
       pendingCollection,
       name,
       paymentLimit,
@@ -839,7 +839,8 @@ router.post('/assignRider', async (req, res) => {
 
     const morningFareTime = moment('04:00', 'HH:mm').tz('Asia/karachi');
     const noonFareTime = moment('17:00', 'HH:mm').tz('Asia/karachi');
-    const lateNightFareTime = moment('19:00', 'HH:mm').tz('Asia/karachi');
+    const nightFareTime = moment('19:00', 'HH:mm').tz('Asia/karachi');
+    const lateNightFareTime = moment('21:00', 'HH:mm').tz('Asia/karachi');
 
     if (
       orderTime.isSame(morningFareTime) ||
@@ -850,9 +851,16 @@ router.post('/assignRider', async (req, res) => {
 
     if (
       orderTime.isSame(noonFareTime) ||
-      orderTime.isBetween(noonFareTime, lateNightFareTime)
+      orderTime.isBetween(noonFareTime, nightFareTime)
     ) {
       req.body.riderFare = nightFare;
+    }
+
+    if (
+      orderTime.isSame(nightFareTime) ||
+      orderTime.isBetween(nightFareTime, lateNightFareTime)
+    ) {
+      req.body.riderFare = lateNightFare;
     }
 
     await Promise.all([
