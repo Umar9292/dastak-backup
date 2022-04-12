e6b054c7739b1d57aa3cad3f18cf0d48fbccd8ae
Umar9292 <umarpromd@gmail.com>
2022-04-12 17:06:17 +0500

    separated one signal configs

--- src/notificationHandler/handler.js
+++ src/notificationHandler/handler.js
@@ -1,5 +1,9 @@
 const { Notification } = require('onesignal-node');
-const { oneSignalClient } = require('../../utils/onSignalConfig');
+const {
+  oneSignalUserClient,
+  oneSignalVendorClient,
+  oneSignalRiderClient,
+} = require('../../utils/onSignalConfig');
 
 exports.notifyAdmin = async (info, msg, whomToSend, toBeSentData) => {
   const notification = new Notification({
@@ -16,7 +20,7 @@ exports.notifyAdmin = async (info, msg, whomToSend, toBeSentData) => {
       'https://res.cloudinary.com/hmwday8rj/image/upload/v1596543000/ios_icon_rrtypi.png',
   });
 
-  const { data } = await oneSignalClient.sendNotification(notification);
+  const { data } = await oneSignalVendorClient.sendNotification(notification);
   return data.id ? console.log(info) : console.log(data.errors[0]);
 };
 
@@ -35,7 +39,7 @@ exports.notifyUser = async (msg, whomToSend, toBeSentData) => {
       'https://res.cloudinary.com/hmwday8rj/image/upload/v1596543000/ios_icon_rrtypi.png',
   });
 
-  const { data } = await oneSignalClient.sendNotification(notification);
+  const { data } = await oneSignalUserClient.sendNotification(notification);
   return data.id ? console.log('User Notified') : console.log(data.errors[0]);
 };
 
@@ -48,13 +52,13 @@ exports.notifyRiders = async (riderName, msg, whomToSend, toBeSentData) => {
     data: toBeSentData,
     ios_sound: 'dastak.wav',
     android_sound: 'dastak',
-    android_channel_id: '89c63241-377d-49b6-8605-271517dc0a71',
+    android_channel_id: '978916b0-393b-4003-a090-405ccab2d321',
     small_icon: 'ic_stat_onesignal_default',
     large_icon:
       'https://res.cloudinary.com/hmwday8rj/image/upload/v1596543000/ios_icon_rrtypi.png',
   });
 
-  const { data } = await oneSignalClient.sendNotification(notification);
+  const { data } = await oneSignalRiderClient.sendNotification(notification);
   return data.id
     ? console.log(`${riderName} Notified`)
     : console.log(data.errors[0]);
--- utils/onSignalConfig.js
+++ utils/onSignalConfig.js
@@ -1,10 +1,28 @@
 const { Client } = require('onesignal-node');
 
-exports.oneSignalClient = new Client({
+exports.oneSignalUserClient = new Client({
   userAuthKey: process.env.ONE_SIGNAL_USER_AUTH_KEY,
 
   app: {
     appAuthKey: process.env.ONE_SIGNAL_APP_AUTH_KEY,
-    appId: process.env.ONE_SIGNAL_APP_ID,
+    appId: process.env.ONE_SIGNAL_USER_APP_ID,
+  },
+});
+
+exports.oneSignalVendorClient = new Client({
+  userAuthKey: process.env.ONE_SIGNAL_USER_AUTH_KEY,
+
+  app: {
+    appAuthKey: process.env.ONE_SIGNAL_APP_AUTH_KEY,
+    appId: process.env.ONE_SIGNAL_VENDOR_APP_ID,
+  },
+});
+
+exports.oneSignalRiderClient = new Client({
+  userAuthKey: process.env.ONE_SIGNAL_USER_AUTH_KEY,
+
+  app: {
+    appAuthKey: process.env.ONE_SIGNAL_APP_AUTH_KEY,
+    appId: process.env.ONE_SIGNAL_RIDER_APP_ID,
   },
 });
