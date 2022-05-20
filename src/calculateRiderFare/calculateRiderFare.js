1085d27169d20c3707c1ae93f70a4621126d5bdb
Haseeb005 <haseeb@healthrix.com>
2022-05-20 16:30:39 +0500

    change rider fares

--- src/calculateRiderFare/calculateRiderFare.js
+++ src/calculateRiderFare/calculateRiderFare.js
@@ -16,31 +16,31 @@ exports.calculateRiderFare = async (
   let riderFare = 0;
 
   if (distance <= 0.8) {
-    riderFare = 45;
-  }
-
-  if (distance > 0.8 && distance <= 1.7) {
     riderFare = 50;
   }
 
-  if (distance > 1.7 && distance <= 2.6) {
+  if (distance > 0.8 && distance <= 1.7) {
     riderFare = 60;
   }
 
-  if (distance > 2.6 && distance <= 3.5) {
-    riderFare = 65;
-  }
-
-  if (distance > 3.5 && distance <= 4.4) {
+  if (distance > 1.7 && distance <= 2.6) {
     riderFare = 70;
   }
 
-  if (distance > 4.4 && distance <= 5.3) {
+  if (distance > 2.6 && distance <= 3.5) {
     riderFare = 75;
   }
 
-  if (distance > 5.3) {
+  if (distance > 3.5 && distance <= 4.4) {
+    riderFare = 80;
+  }
+
+  if (distance > 4.4 && distance <= 5.3) {
     riderFare = 90;
+  }
+
+  if (distance > 5.3) {
+    riderFare = 100;
   }
 
   return riderFare;
