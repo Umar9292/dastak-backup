966e77a01aa78555f42d808fe5d4ba32e1969c8b
Umar9292 <umarpromd@gmail.com>
2022-05-31 15:30:57 +0500

    testing shifts

--- src/routes/rider/shiftEndChecker.js
+++ src/routes/rider/shiftEndChecker.js
@@ -1,4 +1,3 @@
-const { add } = require('lodash');
 const moment = require('moment-timezone/builds/moment-timezone-with-data-2012-2022');
 
 const Users = require('../../models/userModel');
@@ -22,7 +21,7 @@ exports.shiftEndChecker = async () => {
 
       endShift = moment(endShift, 'HH:mm')
         .tz('Asia/Karachi')
-        .add(9, 'hours')
+        .add(1, 'days')
         .subtract(5, 'hours');
 
       console.log(endShift);
