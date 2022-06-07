c0c02201968381fcc5bbcda8c31282293e59fca4
Umar9292 <umarpromd@gmail.com>
2022-06-07 16:43:48 +0500

    fixed a bug in pick up deals

--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -526,7 +526,7 @@ router.post('/pickupDeals', async (req, res) => {
             const productOpening = moment(product.startTime, 'HH:mm')
               .tz('Asia/Karachi')
               .subtract(5, 'hours');
-            let productClosing = moment(product.endtime, 'HH:mm')
+            let productClosing = moment(product.endTime, 'HH:mm')
               .tz('Asia/Karachi')
               .subtract(5, 'hours');
 
