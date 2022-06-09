f1411b2e8a22b4442ca07cfc2af696d5252e5b44
Umar9292 <umarpromd@gmail.com>
2022-06-09 19:33:12 +0500

    removed some logs

--- src/routes/marts/marts.js
+++ src/routes/marts/marts.js
@@ -134,8 +134,6 @@ router.post('/allRestaurants', async (req, res) => {
   try {
     const { lat, long, employee, city, flag } = req.body;
 
-    console.log(req.body);
-
     /* if (city === '') {
       city = await getCity(lat, long);
     }
