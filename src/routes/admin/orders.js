0b3c8d444243eca229e3bac6f20b459fdb74aa60
Umar9292 <umarpromd@gmail.com>
2022-06-21 18:54:32 +0500

    minor update in assign rider

--- src/routes/admin/orders.js
+++ src/routes/admin/orders.js
@@ -886,7 +886,7 @@ router.post('/assignRider', async (req, res) => {
       Orders.findById(orderId),
 
       Users.findById(riderId)
-        .select('pendingCollection name paymentLimit orderCount')
+        .select('pendingCollection name paymentLimit orderCount fareType')
         .lean(),
 
       Orders.find({
