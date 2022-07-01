797bddbc5f20dd0c6043181297523f63cbc83d9e
Umar9292 <umarpromd@gmail.com>
2022-07-01 14:05:03 +0500

    minor changes in restaurant collections

--- src/routes/admin/dashboard/restaurantManagement.js
+++ src/routes/admin/dashboard/restaurantManagement.js
@@ -68,8 +68,8 @@ router.post('/restaurantCollections', async (req, res) => {
         const [orders, restaurant] = await Promise.all([
           Orders.find({
             $or: [
-              { status: 'Delivered', paid: false },
-              { status: 'Rejected', refundToRestaurant: true, paid: false },
+              { status: 'Delivered' },
+              { status: 'Rejected', refundToRestaurant: true },
             ],
             martId,
             city,
