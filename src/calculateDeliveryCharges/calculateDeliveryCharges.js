6b03e6a4d2dbd750936e3f1dbf90b2b901f1f03b
Umar9292 <umarpromd@gmail.com>
2022-07-05 19:00:20 +0500

    minor

--- src/calculateDeliveryCharges/calculateDeliveryCharges.js
+++ src/calculateDeliveryCharges/calculateDeliveryCharges.js
@@ -36,7 +36,7 @@ exports.calculateDeliveryCharges = async (
   }
 
   if (distance > 3.6) {
-    deliveryCharges = 60;
+    deliveryCharges = Object.values(orderFares.deliveryCharges[0])[3];
   }
 
   return deliveryCharges;
