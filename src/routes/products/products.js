18e464df27c00d29d4bdbcc33219099bdeb7257f
Umar9292 <umarpromd@gmail.com>
2022-07-07 16:28:59 +0500

    updated dastak deals now getiting specifications only when the product type is deal.

--- src/routes/products/products.js
+++ src/routes/products/products.js
@@ -631,7 +631,7 @@ router.post('/dastakDeals', async (req, res) => {
 
     await Promise.all(
       openRestaurants.map(async ({ _id: martId }) => {
-        const [restaurant, products, options] = await Promise.all([
+        const [restaurant, products] = await Promise.all([
           Users.findById(martId),
 
           Products.find({
@@ -639,19 +639,17 @@ router.post('/dastakDeals', async (req, res) => {
             dastakDeal: true,
             available: 'in stock',
           }).sort({ price: 1 }),
-
-          Flavours.findOne({ martId }),
         ]);
 
         if (products.length > 0) {
-          const { specifications: flavourSpecifications } = options;
-
           for (const product of products) {
             const details = [];
 
             product.restaurant = restaurant;
 
             if (product.type === 'deal') {
+              const options = await Flavours.findOne({ martId });
+              const { specifications: flavourSpecifications } = options;
               const { specifications } = product;
 
               await Promise.all(
