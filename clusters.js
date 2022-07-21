1187176159ca9201c74cc25a4aa2e5ed95f21641
Umar9292 <umarpromd@gmail.com>
2022-07-21 18:47:19 +0500

    param updates for production

--- clusters.js
+++ clusters.js
@@ -2,11 +2,11 @@
 require('dotenv').config();
 const cluster = require('cluster');
 const { connect } = require('mongoose');
-// const OS = require('os');
+const OS = require('os');
 
 const { dbUrl } = require('./utils/dbUrls');
-// const { shiftEndChecker } = require('./src/routes/rider/shiftEndChecker');
-// const { shiftStartChecker } = require('./src/routes/rider/shiftStartChecker');
+const { shiftEndChecker } = require('./src/routes/rider/shiftEndChecker');
+const { shiftStartChecker } = require('./src/routes/rider/shiftStartChecker');
 
 if (cluster.isMaster) {
   connect(
@@ -21,17 +21,17 @@ if (cluster.isMaster) {
       if (err) {
         console.log(err);
       } else {
-        // setInterval(() => {
-        //   shiftStartChecker();
-        //   shiftEndChecker();
-        // }, 10000);
+        setInterval(() => {
+          shiftStartChecker();
+          shiftEndChecker();
+        }, 10000);
 
         console.log('Connected to database');
       }
     }
   );
 
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < OS.cpus().length; i += 1) {
     cluster.fork();
   }
 
