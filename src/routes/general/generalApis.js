533f0085b6074d80957d270ccecdba48386b541b
Umar9292 <umarpromd@gmail.com>
2022-09-14 19:18:30 +0500

    minor changes

--- src/routes/general/generalApis.js
+++ src/routes/general/generalApis.js
@@ -815,7 +815,7 @@ router.get('/payFastTest', async (_req, res) => {
       MERCHANT_ID: process.env.PAYFAST_MERCHANT_ID,
       SECURED_KEY: process.env.PAYFAST_SECURED_KEY,
       BASKET_ID: '1',
-      TXNAMT: '300',
+      TXNAMT: '50',
     };
 
     const { data } = await axios.post(
