ebc186e58ff75c4a5b726c218bbbeb6702dc61ea
Umar9292 <umarpromd@gmail.com>
2022-09-23 13:45:58 +0500

    updated riders app version.

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -81,7 +81,7 @@ router.post('/ridersCheckVersion', async (req, res) => {
   try {
     const { version } = req.body;
 
-    if (version === '1.6' || version === '1.7') {
+    if (version === '1.6' || version === '1.8') {
       return res.json({ status: '200' });
     }
 
