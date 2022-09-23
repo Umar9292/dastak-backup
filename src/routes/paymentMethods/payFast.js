159dcb2f187a5bc4462cf2e0159b650f77eaac46
Umar9292 <umarpromd@gmail.com>
2022-09-23 16:06:50 +0500

    changes calback for payfast

--- src/routes/paymentMethods/payFast.js
+++ src/routes/paymentMethods/payFast.js
@@ -70,7 +70,7 @@ router.post('/v2/getToken', async (req, res) => {
   }
 });
 
-router.post('/payFast/Callback', async (req, res) => {
+router.post('/payFastCallback', async (req, res) => {
   const { err_code, basket_id, issuer_name, PaymentName } = req.body;
 
   //   const transactionType = O.substring(0, 2);
