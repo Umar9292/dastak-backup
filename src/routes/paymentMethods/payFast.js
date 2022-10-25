0efe84a30faf8372d79edaed056c7489becfb6da
Umar9292 <umarpromd@gmail.com>
2022-10-25 13:51:00 +0500

    removed some logs

--- src/routes/paymentMethods/payFast.js
+++ src/routes/paymentMethods/payFast.js
@@ -72,8 +72,6 @@ router.post('/v2/getToken', async (req, res) => {
 router.post('/payFastCallback', async (req, res) => {
   const { err_code, basket_id, issuer_name, PaymentName } = req.body;
 
-  console.log(req.body);
-
   const transactionType = basket_id.substring(0, 4);
 
   if (transactionType === 'PFTO' && err_code === '000') {
