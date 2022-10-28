82e746e8b94b2e2bbd6188bf1af95d18a706d105
Umar9292 <umarpromd@gmail.com>
2022-10-28 14:08:52 +0500

    enabled online payments

--- src/routes/dastakWallet/dastakWallet.js
+++ src/routes/dastakWallet/dastakWallet.js
@@ -3,30 +3,6 @@ const router = require('express/lib/router')();
 const Users = require('../../models/userModel');
 const WalletHistory = require('../../models/walletHistory');
 
-router.post('/v1/getWallet', async (req, res) => {
-  try {
-    const { userId } = req.body;
-
-    const [{ wallet }, history] = await Promise.all([
-      Users.findById(userId)
-        .select('wallet')
-        .lean(),
-
-      WalletHistory.find({ userId })
-        .sort({ createdAt: -1 })
-        .lean(),
-    ]);
-
-    return res.json({ status: '200', wallet, history });
-  } catch (err) {
-    return res.json({
-      status: '404',
-      error: err.toString(),
-      msg: `Looks like something went wrong on our side. Sorry for the inconvenience.`,
-    });
-  }
-});
-
 router.post('/v2/getWallet', async (req, res) => {
   try {
     const { userId } = req.body;
@@ -41,7 +17,7 @@ router.post('/v2/getWallet', async (req, res) => {
         .lean(),
     ]);
 
-    return res.json({ status: '200', wallet, history, topUp: false });
+    return res.json({ status: '200', wallet, history, topUp: true });
   } catch (err) {
     return res.json({
       status: '404',
--- src/routes/user/deliveryCharges.js
+++ src/routes/user/deliveryCharges.js
@@ -10,7 +10,7 @@ const {
 
 const router = Router();
 
-router.post('/calculateDeliveryCharges', async (req, res) => {
+router.post('/v2/calculateDeliveryCharges', async (req, res) => {
   try {
     const {
       martId,
@@ -66,76 +66,6 @@ router.post('/calculateDeliveryCharges', async (req, res) => {
     return res.json({
       status: '202',
       card: true,
-      platformFee,
-      restaurant,
-      address,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.json({
-      status: '404',
-      error: err.toString(),
-      msg: `Looks like something went wrong on our side. Sorry for the inconvenience.`,
-    });
-  }
-});
-
-router.post('/v2/calculateDeliveryCharges', async (req, res) => {
-  try {
-    const {
-      martId,
-      userId,
-      city,
-      martLatitude,
-      martLongitude,
-      userLatitude,
-      userLongitude,
-    } = req.body;
-
-    const [restaurant, user, { platformFee }] = await Promise.all([
-      Users.findById(martId)
-        .select('-password -__v')
-        .lean(),
-
-      Users.findById(userId)
-        .select('-password -__v')
-        .lean(),
-
-      FaresModel.findOne({ city })
-        .select('platformFee')
-        .lean(),
-    ]);
-
-    const deliveryCharges = await calculateDeliveryCharges(
-      restaurant.city,
-      userLatitude,
-      userLongitude,
-      martLatitude,
-      martLongitude
-    );
-
-    restaurant.deliveryCharges = deliveryCharges;
-    restaurant.password = null;
-
-    let { address } = req.body;
-    if (address === 'Current Location') {
-      address = await getAddress(userLatitude, userLongitude);
-    }
-
-    if (userId !== undefined) {
-      return res.json({
-        status: '200',
-        card: false,
-        platformFee,
-        restaurant,
-        address,
-        user,
-      });
-    }
-
-    return res.json({
-      status: '202',
-      card: false,
       platformFee,
       restaurant,
       address,
