a0bff0466d62529da54cfe578583a683e223bb22
Umar9292 <umarpromd@gmail.com>
2022-11-01 18:10:22 +0500

    testing

--- src/routes/admin/dashboard/restaurantManagement.js
+++ src/routes/admin/dashboard/restaurantManagement.js
@@ -285,8 +285,13 @@ router.post('/restaurantCollections', async (req, res) => {
       })
     );
 
+    console.log(totalOfPlatformFee);
+
     const totalProfit =
       data.reduce((a, b) => a + b.ourProfit, 0) + totalOfPlatformFee;
+
+    console.log(totalProfit);
+
     const amountToPay = data.reduce((a, b) => a + b.totalToPay, 0);
     const totalCollection = data.reduce(
       (a, b) => a + b.totalOfDeliveryOrders,
@@ -658,8 +663,13 @@ router.post('/expensesTillNow', async (req, res) => {
       })
     );
 
+    console.log(totalOfPlatformFee);
+
     const totalProfit =
       data.reduce((a, b) => a + b.ourProfit, 0) + totalOfPlatformFee;
+
+    console.log(totalProfit);
+
     const paidToRiders = data.reduce((a, b) => a + b.ridersFare, 0);
     const paidToRestaurants = data.reduce((a, b) => a + b.totalPaid, 0);
 
