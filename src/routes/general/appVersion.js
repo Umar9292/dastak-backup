8b26556acf782334ec0af678355b6e48e2887622
Umar9292 <umarpromd@gmail.com>
2022-12-15 17:40:49 +0500

    updated user app versions

--- src/routes/general/appVersion.js
+++ src/routes/general/appVersion.js
@@ -12,7 +12,7 @@ router.post('/checkVersion', async (req, res) => {
 
     const { cities } = await CitiesModel.findOne({}).lean();
 
-    if (platform === 'ios' && version === '2.2.0') {
+    if (platform === 'ios' && (version === '2.2.0' || version === '3.0.0')) {
       return res.json({ status: '200', url, cities, showCategories: 'false' });
     }
 
@@ -22,7 +22,7 @@ router.post('/checkVersion', async (req, res) => {
       });
     } */
 
-    if (platform === 'ios' && version !== '2.2.0') {
+    if (platform === 'ios' && (version !== '2.2.0' || version !== '3.0.0')) {
       return res.json({
         status: '404',
         msg: `A new update is now available which includes performance improvements. Kindly update your app, it won't take much of your time. Thankyou`,
@@ -30,7 +30,10 @@ router.post('/checkVersion', async (req, res) => {
       });
     }
 
-    if (platform === 'android' && version === '2.2.0') {
+    if (
+      platform === 'android' &&
+      (version === '2.2.0' || version === '3.0.0')
+    ) {
       return res.json({ status: '200', url, cities, showCategories: 'false' });
     }
 
@@ -40,7 +43,10 @@ router.post('/checkVersion', async (req, res) => {
       });
     } */
 
-    if (platform === 'android' && version !== '2.2.0') {
+    if (
+      platform === 'android' &&
+      (version !== '2.2.0' || version !== '3.0.0')
+    ) {
       return res.json({
         status: '404',
         msg: `A new update is now available. kindly update your App to get the best experience.`,
